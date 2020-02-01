26ea10c1f431dad45c322a3d2f8dbfc3d49b179c
liangyu <liangyu722@gmail.com>
2020-02-01 19:26:27 +1100

    Remove retrofit coroutine adapter

--- app/src/main/java/com/github/liangyu/employee/data/remote/network/EmployeeService.kt
+++ app/src/main/java/com/github/liangyu/employee/data/remote/network/EmployeeService.kt
@@ -1,11 +1,10 @@
 package com.github.liangyu.employee.data.remote.network
 
 import com.github.liangyu.employee.data.remote.network.response.EmployeeResponse
-import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 
 interface EmployeeService {
 
     @GET("employees.json")
-    fun employeesAsync(): Deferred<EmployeeResponse>
+    suspend fun employeesAsync(): EmployeeResponse
 }
