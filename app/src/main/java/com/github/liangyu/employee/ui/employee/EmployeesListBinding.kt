a2c1eb129a18daff838c5f81b3996c193a58e74d
liangyu <liangyu722@gmail.com>
2020-02-06 10:47:32 +1100

    Add Glide and binding for image url

--- app/src/main/java/com/github/liangyu/employee/ui/employee/EmployeesListBinding.kt
+++ app/src/main/java/com/github/liangyu/employee/ui/employee/EmployeesListBinding.kt
@@ -1,10 +1,25 @@
 package com.github.liangyu.employee.ui.employee
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.github.liangyu.employee.model.Employee
 
 @BindingAdapter("app:items")
 fun setItems(listView: RecyclerView, items: List<Employee>) {
     (listView.adapter as EmployeesAdapter).submitList(items)
+}
+
+@BindingAdapter("app:imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(view)
+    }
 }
